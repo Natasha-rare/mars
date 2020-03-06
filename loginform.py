6c10ce0297c7e10fafe9f1d51f8c15e80347306d
N@t@sh@ <natasha_ea5@mail.ru>
2020-03-06 17:26:10 +0300

    jobs.append

--- loginform.py
+++ loginform.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+import datetime
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField
 from wtforms.validators import DataRequired
 
 
@@ -8,3 +9,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+class JobsForm(FlaskForm):
+    team_leader = IntegerField('Капитан', validators=[DataRequired()])
+    job = StringField('Название работы', validators=[DataRequired()])
+    work_size = IntegerField('Объем работы', validators=[DataRequired()])
+    collaborators = StringField('Участники', validators=[DataRequired()])
+    is_finished = BooleanField('Статус', validators=[DataRequired()], default=False)
